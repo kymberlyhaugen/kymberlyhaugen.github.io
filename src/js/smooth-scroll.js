f24d2302f76bad759e4c522009a648240fbc74d0
kymberlyhaugen <55845601+kymberlyhaugen@users.noreply.github.com>
2020-01-04 22:05:19 -0600

    Footer background

--- src/js/smooth-scroll.js
+++ src/js/smooth-scroll.js
@@ -3,7 +3,8 @@ import SmoothScroll from 'smooth-scroll/dist/smooth-scroll.polyfills.js';
 export function createSmoothScroll() {
     var scroll = new SmoothScroll('a[href*="#"]', {
         header: '.sticky-header',
-        speed: 500,
+        speed: 750,
+	    speedAsDuration: true,
         offset: 25
     });
 }
